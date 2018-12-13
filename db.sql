a1bf9e8780de1e7259171d82c48d5ad6a869818c
Austin Stotts <austintstotts@gmail.com>
2018-12-13 15:26:23 -0600

    created database

---
+++ db.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS chat;
+CREATE DATABASE chat;
+USE chat;
+
+-- messages
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages`
+(
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR (24) NOT NULL,
+  `message` VARCHAR(255) NOT NULL,
+  `created` TIMESTAMP NOT NULL,
+  PRIMARY KEY(`id`)
+);
+
+-- users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`
+(
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(24) NOT NULL,
+  `password` VARCHAR(24) NOT NULL,
+  PRIMARY KEY(`id`)
+);
